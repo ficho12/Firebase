5e47a6813d39be3b84c77b0dc8955d465c8f2394
Fiz <fizreyarmesto@gmail.com>
2023-07-01 20:03:54 +0200

    v.2 HTTP function added

--- functions/index.js
+++ functions/index.js
@@ -1,46 +1,94 @@
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
+
 admin.initializeApp();
 
-const db = admin.firestore();
-
-exports.updateGlobalHighScores = functions.pubsub
-    .schedule("every 5 minutes")
+exports.updateGlobalHighScores = functions.pubsub.schedule("every 15 minutes")
     .onRun(async (context) => {
-      // Clear the existing GlobalHighScores collection
-      // await db.collection("GlobalHighScores").doc("scores").delete();
-	  await db.collection("GlobalHighScores").getdocs().delete();
+      const db = admin.firestore();
 
-      // Fetch all the user UIDs from ListOfCollections
-      const listOfCollectionsDoc = await db.collection("ListOfCollections")
-          .doc("ListOfCollections").get();
-      const userUIDs = listOfCollectionsDoc.data().uids;
-      // Fetch the high scores for each user and store them in an array
-      const highScoresPromises = userUIDs.map(async (uid) => {
-        const userScores = await db.collection(uid)
-            .orderBy("Final Score", "desc").limit(5).get();
-        return {uid, scores: userScores.docs.map((doc) =>
-          doc.data().HighScore)};
-      });
+      try {
+        // Get the list of user UIDs
+        const listOfCollectionsDoc = await db.collection("ListOfCollections")
+            .doc("ListOfCollections").get();
+        const uidList = listOfCollectionsDoc.data().UIDs;
 
-      const highScores = await Promise.all(highScoresPromises);
+        // Fetch the highest scores for each user
+        const promises = uidList.map(async (uid) => {
+          const userScoresRef = db.collection(uid);
+          const userScoresQuery = userScoresRef
+              .orderBy("Final Score", "desc").limit(5);
+          const userScoresSnapshot = await userScoresQuery.get();
 
-      // Sort the high scores in descending order
-      highScores.sort((a, b) => b.scores[0] - a.scores[0]);
+          const userScores = [];
+          userScoresSnapshot.forEach((doc) => {
+            const score = doc.data().FinalScore;
+            const user = doc.data().email;
+            userScores.push({score, user});
+          });
 
-      // Update the GlobalHighScores document with the top 5 scores
-      const globalHighScoresDoc = db.collection("GlobalHighScores")
-          .doc("scores");
-      for (let i = 0; i < Math.min(highScores.length, 5); i++) {
-        await globalHighScoresDoc.set(highScores[i], {merge: true});
+          return userScores;
+        });
+
+        const highestScoresPerUser = await Promise.all(promises);
+
+        // Combine and sort all the highest scores
+        const allScores = highestScoresPerUser.flat();
+        allScores.sort((a, b) => b.score - a.score);
+        const top5Scores = allScores.slice(0, 5);
+
+        // Update the GlobalHighScores document
+        const globalHighScoresRef = db.collection("GlobalHighScore")
+            .doc("HighScores");
+        await globalHighScoresRef.set({scores: top5Scores});
+
+        return null;
+      } catch (error) {
+        console.error("Error updating GlobalHighScore:", error);
+        throw new Error("Failed to update GlobalHighScore");
       }
-
-      console.log("GlobalHighScores updated successfully!");
-      return null;
     });
 
-// Export the function to use in the Firebase CLI
-// module.exports = {
-// updateGlobalHighScores,
-// s};
+exports.updateGlobalHighScoresHTTPS = functions.https
+    .onRequest(async (req, res) => {
+      const db = admin.firestore();
 
+      try {
+        // Get the list of user UIDs
+        const listOfCollectionsDoc = await db.collection("ListOfCollections")
+            .doc("ListOfCollections").get();
+        const uidList = listOfCollectionsDoc.data().UIDs;
+        // Fetch the highest scores for each user
+        const promises = uidList.map(async (uid) => {
+          const userScoresRef = db.collection(uid);
+          const userScoresQuery = userScoresRef
+              .orderBy("Final Score", "desc").limit(5);
+          const userScoresSnapshot = await userScoresQuery.get();
+
+          const userScores = [];
+          userScoresSnapshot.forEach((doc) => {
+            const score = doc.data().FinalScore;
+            const user = doc.data().email;
+            userScores.push({score, user});
+          });
+
+          return userScores;
+        });
+
+        const highestScoresPerUser = await Promise.all(promises);
+        // Combine and sort all the highest scores
+        const allScores = highestScoresPerUser.flat();
+        allScores.sort((a, b) => b.score - a.score);
+        const top5Scores = allScores.slice(0, 5);
+
+        // Update the GlobalHighScores document
+        const globalHighScoresRef = db.collection("GlobalHighScore")
+            .doc("HighScores");
+        await globalHighScoresRef.set({scores: top5Scores});
+
+        res.status(200).send("GlobalHighScores updated successfully.");
+      } catch (error) {
+        console.error("Error updating GlobalHighScore:", error);
+        res.status(500).send("Failed to update GlobalHighScore.");
+      }
+    });
