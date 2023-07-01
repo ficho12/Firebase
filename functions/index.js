4a282041381bd8d30b51e58de4529c4834efa4f7
Fiz <fizreyarmesto@gmail.com>
2023-07-01 17:32:20 +0200

    first commit

---
+++ functions/index.js
@@ -0,0 +1,8 @@
+const functions = require("firebase-functions");
+
+// http request 1
+exports.randomNumber = functions.https.onRequest((request, response) => {
+  const number = Math.round(Math.random() * 100);
+  response.send(number.toString());
+});
+
